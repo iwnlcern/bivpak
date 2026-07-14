7c3861a9f3ebfbc734d8470dccfdfec5f2dfb3d5
Jack Li <jack@Jacks-MacBook-Pro.local>
2026-07-14 01:55:12 -0700

    build: guard all Clang-only diagnostic suppressions

--- src/core/manifest/checksums.cpp
+++ src/core/manifest/checksums.cpp
@@ -2,12 +2,12 @@
 
 #include <string_view>
 
-#if defined(__GNUC__)
+#if defined(__clang__)
 #pragma GCC diagnostic push
 #pragma GCC diagnostic ignored "-Wdeprecated-literal-operator"
 #endif
 #include <simdjson.h>
-#if defined(__GNUC__)
+#if defined(__clang__)
 #pragma GCC diagnostic pop
 #endif
 
